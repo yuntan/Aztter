ba62f5bffbe5678c45e9277504b69e4f5d61f862
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2013-11-18 23:19:30 +0900

    use QQmlApplicationEngine instead of QtQuick2ApplicationViewer

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
+#include <QtQml>
+#include <QtQuick/QQuickView>
+#include <QtCore/QString>
+
+#ifndef QT_NO_WIDGETS
+#include <QtWidgets/QApplication>
+#else
 #include <QtGui/QGuiApplication>
-#include "qtquick2applicationviewer.h"
+#endif
+
+#ifndef QT_NO_WIDGETS
+#define Application QApplication
+#else
+#define Application QGuiApplication
+#endif
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
-
-    QtQuick2ApplicationViewer viewer;
-    viewer.setMainQmlFile(QStringLiteral("qml/main.qml"));
-//    viewer.setSource(QUrl("qrc:/main.qml"));
-    viewer.showExpanded();
-
+    Application app(argc, argv);
+    QQmlApplicationEngine engine(QUrl("qml/main.qml"));
+    QObject *topLevel = engine.rootObjects().value(0);
+    QQuickWindow *window = qobject_cast<QQuickWindow *>(topLevel);
+    if ( !window ) {
+        qWarning("Error: Your root item has to be a Window.");
+        return -1;
+    }
+    window->show();
     return app.exec();
 }
