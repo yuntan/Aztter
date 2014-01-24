1af2f21bdc1eea09e90304c6d3131517d379491d
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2014-01-25 03:06:50 +0900

    disable debug outputs

--- js/Utils.js
+++ js/Utils.js
@@ -4,15 +4,14 @@ var monthMap = { "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3,
     "May": 4, "Jun": 5, "Jul": 6, "Aug": 7,
     "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11 }
 
-// returns local time
 function parseCreatedAt(createdAt) {
-    console.log("createdAt = " + createdAt)
+//    console.log("createdAt = " + createdAt)
     var array1 = createdAt.split(' ')
     var array2 = array1[3].split(':')
 
     var ret = new Date()
     var offset = ret.getTimezoneOffset() / 60
-    console.log("timezone offset = " + offset)
+//    console.log("timezone offset = " + offset)
     if(array2[0] - offset < 0) {
         array1[2]--
         array2[0] += (24 - offset)
@@ -27,14 +26,14 @@ function parseCreatedAt(createdAt) {
 
     ret.setFullYear(array1[5], monthMap[array1[1]], array1[2])
     ret.setHours(array2[0], array2[1], array2[2])
-    console.log("ret = " + ret)
+//    console.log("ret = " + ret)
     return ret
 }
 
 function dateToStr(createdAt) {
     // FIXME
     var now = new Date()
-    console.log("now = " + now.getTime() + " createdAt = " + createdAt.getTime())
+//    console.log("now = " + now.getTime() + " createdAt = " + createdAt.getTime())
     var diff = Math.floor((now.getTime() - createdAt.getTime()) / 1000)
     if(diff >= 86400)
         return Math.floor(diff / 86400) + qsTr(" days ago")
